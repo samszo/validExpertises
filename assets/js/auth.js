c86dc4ff9775c39e86b9b9d606e25f93a71ad8e2
samszo <samszon@gmail.com>
2026-04-08 18:20:26 +0200

    feat: add initial implementation of the validation interface for expertises with settings modal and keyword management

---
+++ assets/js/auth.js
@@ -0,0 +1,42 @@
+import {omk} from './omk.js';
+
+export class auth {
+    constructor(params) {
+        var me = this;
+        this.modal;
+        this.m;
+        this.navbar = params.navbar ? params.navbar : 'navbarMain';
+        this.apiOmk = params.apiOmk ? params.apiOmk : false; 
+        this.mail = params.mail ? params.mail : false;
+        this.ident = params.ident ? params.ident : false;
+        this.key = params.key ? params.key : false;
+        this.omk = false;
+        this.keyGitHub = params.keyGitHub ? params.keyGitHub : false;        
+        this.octo = params.octo ? params.octo : false;        
+        this.userAdmin=false;
+        this.user=false;
+                        
+        this.init = function () {
+            console.log('init');
+        }
+            
+        this.getUser = function (cb){
+
+            //vérifie la connexion à OMK
+            if(me.apiOmk) me.apiOmk += me.apiOmk.slice(-1)=='/' ? "" : "/";
+            me.omk = new omk({'api':me.apiOmk,'key':me.key,'ident':me.ident,'mail':me.mail});
+            me.omk.getUser(u=>{
+                if(!u){
+                    me.user = false;
+                    me.omk = false;                                                                     
+                }else {
+                    me.user = u;
+                    me.userAdmin = me.user["o:role"] == 'global_admin';            
+                    me.user.id=me.user['o:id'];
+                }
+            });
+        }
+
+        this.init();
+    }
+}
---
+++ assets/js/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+        mail:'samuel.szoniecky@univ-paris8.fr',
+        apiOmk:'http://localhost/omk_paragraphe/api/',
+        ident: 'oc1IaQrD7JGkIWSSrsWSHaprrwfXT11C',
+        key: 'h6zoRTnpUrwruIFk2D734AuSExLrAhG0',
+        creatorId:'7457',
+        "navbar":null    
+}
