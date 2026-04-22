b1e4b223f92f40d2259ce40278a7b5198830daf7
samszo <samszon@gmail.com>
2026-04-22 08:13:08 +0200

    feat: mettre à jour l'identifiant d'authentification et modifier la vérification de la clé dans le formulaire de paramètres

--- assets/js/authParams.js
+++ assets/js/authParams.js
@@ -1,7 +1,7 @@
 export const pa = {
         mail:'valorisation@univ-paris8.fr',
         apiOmk:'https://valorisation.humanum-p8.fr/api/',
-        ident: 'sHtMnUFdtPxCUh4o7jEcIttblz72sqTA',
+        ident: 'YYT4dJSyzB3x30eeCU3V3eloXhWXBIVa',
         key: '',
         creatorId:'7457',
         personTypeAllow:'valo:EnseignantChercheur',
